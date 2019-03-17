40b160fccfb484c96704c128c3f23d45a306e828
Krisana Tubsaithong <mosthsiza@gmail.com>
2019-03-17 11:58:05 +0700

    Updata avatar

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  //items = ["Krisana","Nitipong","Sathaporn","Terajeet","Tanatat"];
 
+  items = [
+    {"name" : "Krisana","tel" : "0858494472","avatar" : "avatar icon1.png" },
+    {"name" : "Tanatat","tel" : "0909404504","avatar" : "avatar icon2.png" },
+    {"name" : "Sompot","tel" : "0854231518","avatar" : "avatar icon3.jpg" },
+    {"name" : "Nitipong","tel" : "0909523457","avatar" : "avatar icon4.png" },
+    {"name" : "Sathaporn","tel" : "0637826654","avatar" : "avatar icon5.png" }
+  ];
+  itemSelected(item : string){
+    console.log("Selected Item : ",item);
+  }
   constructor(public navCtrl: NavController) {
-
   }
 
 }
